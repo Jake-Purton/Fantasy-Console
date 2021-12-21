e15a9d000d26b6b9e6bf1bfeb975ec834fe8c3a2
Ian Purton <ian.purton@gmail.com>
2021-12-21 17:07:51 +0000

    formatted.

--- src/lib.rs
+++ src/lib.rs
@@ -2,29 +2,24 @@ mod wasm4;
 use wasm4::*;
 
 #[no_mangle]
-fn start () {
+fn start() {
     trace("I've been called");
     unsafe {
-        *PALETTE = [
-            0xff0000,
-            0x00ff00,
-            0x0000ff,
-            0x000000,
-        ];
+        *PALETTE = [0xff0000, 0x00ff00, 0x0000ff, 0x000000];
     }
 }
+
 #[no_mangle]
-fn update () {
+fn update() {
     let mut colour = 1;
     for n in 0..160 {
-        for x in 0..160{
+        for x in 0..160 {
             colour = colour + 1;
-            if colour > 3{
+            if colour > 3 {
                 colour = 1
             }
             unsafe { *DRAW_COLORS = colour }
             rect(n, x, 1, 1);
         }
     }
-
 }
