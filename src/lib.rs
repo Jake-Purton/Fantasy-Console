942f4d0be5efbe31644e07cedff3ff0470e4c5ef
apersononthemac <jakecapurton@gmail.com>
2021-12-21 16:23:57 +0000

    several squares of different colours

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,26 @@ mod wasm4;
 use wasm4::*;
 
 #[no_mangle]
+fn start () {
+    trace("I've been called");
+    unsafe {
+        *PALETTE = [
+            0xd2e7af,
+            0x5aded9,
+            0x2979b6,
+            0x081935,
+        ];
+    }
+}
+
+#[no_mangle]
 fn update () {
+    unsafe { *DRAW_COLORS = 2 }
     rect(10, 10, 32, 32);
+    unsafe { *DRAW_COLORS = 3 }
+    rect(52, 10, 32, 32);
+    unsafe { *DRAW_COLORS = 4 }
+    rect(94, 10, 32, 32);
+
+    rect(100,100,1,1)
 }
