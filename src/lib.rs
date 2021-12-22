a8ddd235ba70658f299172476c31a41febf2d71b
Ian Purton <ian.purton@gmail.com>
2021-12-22 11:17:17 +0000

    Refactor.

--- src/lib.rs
+++ src/lib.rs
@@ -1,22 +1,32 @@
 mod wasm4;
 use wasm4::*;
 
-pub struct Ball1 {
+pub struct Ball {
     pub ball_x: i32,
     pub ball_y: i32,
     pub dx: i8,
     pub dy: i8
 
-}pub struct Ball2 {
-    pub ball_x: i32,
-    pub ball_y: i32,
-    pub dx: i8,
-    pub dy: i8
+}
+
+impl Ball {
+    pub fn update(&mut self) {
+        self.ball_x += self.dx as i32;
+        self.ball_y += self.dy as i32;
+
+        if self.ball_x > 159 || self.ball_x < 1 {
+            self.dx = self.dx * -1;
+        }
+
+        if self.ball_y > 159 || self.ball_y < 1 {
+            self.dy = self.dy * -1;
+        }
+    }
 }
 
 // STATIC FOR BALL 1
 
-static mut BALL1: Ball1 = Ball1 {
+static mut BALL1: Ball = Ball {
     ball_x: 44,
     ball_y: 139,
     dx: 3,
@@ -25,7 +35,7 @@ static mut BALL1: Ball1 = Ball1 {
 
 // STATIC FOR BALL 2
 
-static mut BALL2: Ball2 = Ball2 {
+static mut BALL2: Ball = Ball {
     ball_x: 79,
     ball_y: 92,
     dx: 1,
@@ -39,9 +49,6 @@ fn start() {
         *PALETTE = [0x000000, 0xffffff, 0xeb6b6f, 0x7c3f58];
     }
 
-    let _ball1 = unsafe { &mut BALL1 };
-    let _ball2 = unsafe { &mut BALL2 };
-
 }
 
 
@@ -51,32 +58,10 @@ fn update() {
     // MOVES THE FIRST BALL
 
     let ball1 = unsafe { &mut BALL1 };
-
-    ball1.ball_x += ball1.dx as i32;
-    ball1.ball_y += ball1.dy as i32;
-
-    if ball1.ball_x > 159 || ball1.ball_x < 1 {
-        ball1.dx = ball1.dx * -1;
-    }
-
-    if ball1.ball_y > 159 || ball1.ball_y < 1 {
-        ball1.dy = ball1.dy * -1;
-    }
-
-    // MOVES THE SECOND BALL
-    
     let ball2 = unsafe { &mut BALL2 };
 
-    ball2.ball_x += ball2.dx as i32;
-    ball2.ball_y += ball2.dy as i32;
-
-    if ball2.ball_x > 159 || ball2.ball_x < 1 {
-        ball2.dx = ball2.dx * -1;
-    }
-
-    if ball2.ball_y > 159 || ball2.ball_y < 1 {
-        ball2.dy = ball2.dy * -1;
-    }
+    ball1.update();
+    ball2.update();
 
     // DRAWS A LINE BETWEEN THE TWO BALLS
 
