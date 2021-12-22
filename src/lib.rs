e1160771cdab219011f57eee749c57ddb761f94c
Ian Purton <ian.purton@gmail.com>
2021-12-22 10:25:12 +0000

    Fix bounce.

--- src/lib.rs
+++ src/lib.rs
@@ -2,8 +2,8 @@ mod wasm4;
 use wasm4::*;
 
 pub struct Ball {
-    pub ball_x: u8,
-    pub ball_y: u8,
+    pub ball_x: i32,
+    pub ball_y: i32,
     pub dx: i8,
     pub dy: i8
 }
@@ -26,12 +26,18 @@ fn start() {
 fn update() {
     let ball = unsafe { &mut BALL };
 
-    ball.ball_x = (ball.ball_x as i8 + ball.dx) as u8;
-    ball.ball_y = (ball.ball_y as i8 + ball.dy) as u8;
+    ball.ball_x += ball.dx as i32;
+    ball.ball_y += ball.dy as i32;
 
     rect(ball.ball_x as i32, ball.ball_y as i32, 1, 1);
 
     if ball.ball_x > 159 || ball.ball_x < 1 {
         ball.dx = ball.dx * -1;
+        trace("here")
+    }
+
+    if ball.ball_y > 159 || ball.ball_y < 1 {
+        ball.dy = ball.dy * -1;
+        trace("here")
     }
 }
