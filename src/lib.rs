e5d6af33e890975a4920f8d4a29de7936b311dda
apersononthemac <jakecapurton@gmail.com>
2021-12-22 11:11:52 +0000

    bouncy line.

--- src/lib.rs
+++ src/lib.rs
@@ -1,50 +1,85 @@
 mod wasm4;
 use wasm4::*;
-use quad_rand as qrand;
 
-pub struct Ball {
+pub struct Ball1 {
+    pub ball_x: i32,
+    pub ball_y: i32,
+    pub dx: i8,
+    pub dy: i8
+
+}pub struct Ball2 {
     pub ball_x: i32,
     pub ball_y: i32,
     pub dx: i8,
     pub dy: i8
 }
 
-static mut BALL: Ball = Ball {
-    ball_x: 0,
-    ball_y: 0,
+// STATIC FOR BALL 1
+
+static mut BALL1: Ball1 = Ball1 {
+    ball_x: 44,
+    ball_y: 139,
+    dx: 3,
+    dy: 2
+};
+
+// STATIC FOR BALL 2
+
+static mut BALL2: Ball2 = Ball2 {
+    ball_x: 79,
+    ball_y: 92,
     dx: 1,
-    dy: 1
+    dy: -3
 };
 
 #[no_mangle]
 fn start() {
+    unsafe { *DRAW_COLORS = 2 }
     unsafe {
-        *PALETTE = [0xfff6d3, 0xf9a875, 0xeb6b6f, 0x7c3f58];
+        *PALETTE = [0x000000, 0xffffff, 0xeb6b6f, 0x7c3f58];
     }
 
-    let ball = unsafe { &mut BALL };
+    let _ball1 = unsafe { &mut BALL1 };
+    let _ball2 = unsafe { &mut BALL2 };
 
-    ball.ball_x = qrand::gen_range(0, 159);
-    ball.ball_y = qrand::gen_range(0, 159);
 }
+
 
 #[no_mangle]
 fn update() {
 
-    let ball = unsafe { &mut BALL };
+    // MOVES THE FIRST BALL
 
-    ball.ball_x += ball.dx as i32;
-    ball.ball_y += ball.dy as i32;
+    let ball1 = unsafe { &mut BALL1 };
 
-    rect(ball.ball_x as i32, ball.ball_y as i32, 1, 1);
+    ball1.ball_x += ball1.dx as i32;
+    ball1.ball_y += ball1.dy as i32;
 
-    if ball.ball_x > 159 || ball.ball_x < 1 {
-        ball.dx = ball.dx * -1;
-        trace("here")
+    if ball1.ball_x > 159 || ball1.ball_x < 1 {
+        ball1.dx = ball1.dx * -1;
     }
 
-    if ball.ball_y > 159 || ball.ball_y < 1 {
-        ball.dy = ball.dy * -1;
-        trace("here")
+    if ball1.ball_y > 159 || ball1.ball_y < 1 {
+        ball1.dy = ball1.dy * -1;
     }
+
+    // MOVES THE SECOND BALL
+    
+    let ball2 = unsafe { &mut BALL2 };
+
+    ball2.ball_x += ball2.dx as i32;
+    ball2.ball_y += ball2.dy as i32;
+
+    if ball2.ball_x > 159 || ball2.ball_x < 1 {
+        ball2.dx = ball2.dx * -1;
+    }
+
+    if ball2.ball_y > 159 || ball2.ball_y < 1 {
+        ball2.dy = ball2.dy * -1;
+    }
+
+    // DRAWS A LINE BETWEEN THE TWO BALLS
+
+    line(ball1.ball_x, ball1.ball_y, ball2.ball_x, ball2.ball_y)
+
 }
